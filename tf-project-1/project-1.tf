d4746b9ea161a324fe5a60c4a7ca6d84e4c9bf15
Isa M <isa_xo@hotmail.com>
2024-12-05 16:14:49 +0000

    Added commenting

--- tf-project-1/project-1.tf
+++ tf-project-1/project-1.tf
@@ -1,8 +1,11 @@
+## Provider Information
 provider "aws" {
     region  = "eu-west-2"
 }
 
-resource "aws_instance" "my-first-server" {
+
+## AWS Single Test Instance 
+resource "aws_instance" "my-test-server" {
     ami           = "ami-0c76bd4bd302b30ec"
     instance_type = "t2.micro"
     tags = {
@@ -10,3 +13,4 @@ resource "aws_instance" "my-first-server" {
     Type = "t2.micro"
      }
 }
+
