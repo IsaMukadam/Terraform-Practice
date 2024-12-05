79003e0c72b4f9b9ff98cb2001a5cd004e9ebd22
Isa M <isa_xo@hotmail.com>
2024-12-05 17:02:16 +0000

    creating a vpc with 3 subnets

---
+++ project-2/create-vpc.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+    region  = "eu-west-2"
+}
+
+resource "aws_vpc" "test-vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+      Name = "Test VPC"
+    }
+}
+
+resource "aws_subnet" "subnet-1" {
+    vpc_id     = aws_vpc.test-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "eu-west-2a"
+
+    tags = {
+      Name = "Subnet-1" 
+    } 
+}
+
+resource "aws_subnet" "subnet-2" {
+    vpc_id     = aws_vpc.test-vpc.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "eu-west-2b"
+
+    tags = {
+      Name = "Subnet-2" 
+    } 
+} 
+
+resource "aws_subnet" "subnet-3" {
+    vpc_id     = aws_vpc.test-vpc.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "eu-west-2c"
+
+    tags = {
+      Name = "Subnet-3" 
+    } 
+} 
