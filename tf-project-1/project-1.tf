027229a00122bdb63ab6eeeb4673a392708ae77f
Isa M <isa_xo@hotmail.com>
2024-12-05 16:09:35 +0000

    my first server project + gitignore

--- tf-project-1/project-1.tf
+++ tf-project-1/project-1.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-    version = "~> 2.0"
     region  = "eu-west-2"
 }
 
+resource "aws_instance" "my-first-server" {
+    ami           = "ami-0c76bd4bd302b30ec"
+    instance_type = "t2.micro"
+    tags = {
+    Name = "Linux Server"
+    Type = "t2.micro"
+     }
+}
