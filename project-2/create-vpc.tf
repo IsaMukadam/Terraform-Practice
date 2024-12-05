8ce72e727b72be7502a4d5774f706fc99cf78ab7
Isa M <isa_xo@hotmail.com>
2024-12-05 17:05:03 +0000

    commenting + creating project 3

--- project-2/create-vpc.tf
+++ project-2/create-vpc.tf
@@ -1,7 +1,9 @@
+# Config
 provider "aws" {
     region  = "eu-west-2"
 }
 
+## VPC
 resource "aws_vpc" "test-vpc" {
     cidr_block = "10.0.0.0/16"
 
@@ -10,6 +12,7 @@ resource "aws_vpc" "test-vpc" {
     }
 }
 
+## Subnet 1
 resource "aws_subnet" "subnet-1" {
     vpc_id     = aws_vpc.test-vpc.id
     cidr_block = "10.0.1.0/24"
@@ -20,6 +23,7 @@ resource "aws_subnet" "subnet-1" {
     } 
 }
 
+## Subnet 2
 resource "aws_subnet" "subnet-2" {
     vpc_id     = aws_vpc.test-vpc.id
     cidr_block = "10.0.2.0/24"
@@ -30,6 +34,7 @@ resource "aws_subnet" "subnet-2" {
     } 
 } 
 
+## Subnet 3
 resource "aws_subnet" "subnet-3" {
     vpc_id     = aws_vpc.test-vpc.id
     cidr_block = "10.0.3.0/24"
